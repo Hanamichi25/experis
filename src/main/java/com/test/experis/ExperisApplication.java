93753bc4f8c5d45ced8e5de0a80ee1be9860ba79
Contartista Accion Plus-Daniel Ricardo Martin Niño <martdani@colpatria.com>
2019-04-29 10:15:33 -0500

    First commit with basic project

---
+++ src/main/java/com/test/experis/ExperisApplication.java
@@ -0,0 +1,13 @@
+package com.test.experis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/experis/ExperisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.experis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExperisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
